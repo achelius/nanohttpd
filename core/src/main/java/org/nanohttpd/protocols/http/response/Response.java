f3cbeee2f90ebd5a8612b97b3a0b2568dcd61c50
Achelius <rompamail2@gmail.com>
2017-10-02 00:40:05 +0200

    added cors header

--- core/src/main/java/org/nanohttpd/protocols/http/response/Response.java
+++ core/src/main/java/org/nanohttpd/protocols/http/response/Response.java
@@ -272,7 +272,6 @@ public class Response implements Closeable {
 		}
 	}
 
-	@SuppressWarnings("static-method")
 	protected void printHeader(PrintWriter pw, String key, String value) {
 		pw.append(key).append(": ").append(value).append("\r\n");
 	}
